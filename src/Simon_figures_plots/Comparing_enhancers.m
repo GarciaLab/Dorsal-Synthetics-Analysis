f1d7be09f1aff0a514bbc64cfd583ce6732eb858
Simon Alamos <simon.alamos@berkeley.edu>
2021-01-02 11:31:43 -0800

    minor update

--- src/Simon_figures_plots/Comparing_enhancers.m
+++ src/Simon_figures_plots/Comparing_enhancers.m
@@ -3,86 +3,92 @@ function Comparing_enhancers
 %% grab results
 clear all
 dorsalResultsDatabase = createDorsalResultsDatabase;
-dlfluobins = dorsalResultsDatabase.dorsalFluoBins; dlfluobins = dlfluobins(1:37);
+
+dlfluobins_1 = dorsalResultsDatabase.dorsalFluoBins; dlfluobins_1 = dlfluobins_1(1:37);
 %cond1 = [dorsalResultsDatabase.DataType]=="1Dg11" & [dorsalResultsDatabase.nc]==12;
 cond1 = contains([dorsalResultsDatabase.DataType],'1Dg11_2xDl_FFF') & [dorsalResultsDatabase.nc]==12;
-frac2x = dorsalResultsDatabase.meanFracFluoEmbryo(cond1);
-sefrac2x = dorsalResultsDatabase.seFracFluoEmbryo(cond1);
-max2x = dorsalResultsDatabase.meanAllMaxFluoEmbryo(cond1);
-semax2x = dorsalResultsDatabase.seAllMaxFluoEmbryo(cond1);
-accum2x = dorsalResultsDatabase.meanallmrnasEmbryo(cond1);
-seaccum2x = dorsalResultsDatabase.seallmrnasEmbryo(cond1);
-turnOn2x = dorsalResultsDatabase.meanTurnOnsEmbryo(cond1);
-seTurnON2x = dorsalResultsDatabase.seTurnOnsEmbryo(cond1);
+frac_1 = dorsalResultsDatabase.meanFracFluoEmbryo(cond1);
+sefrac_1 = dorsalResultsDatabase.seFracFluoEmbryo(cond1);
+max_1 = dorsalResultsDatabase.meanAllMaxFluoEmbryo(cond1);
+semax_1 = dorsalResultsDatabase.seAllMaxFluoEmbryo(cond1);
+accum_1 = dorsalResultsDatabase.meanallmrnasEmbryo(cond1);
+seaccum_1 = dorsalResultsDatabase.seallmrnasEmbryo(cond1);
+turnOn_1 = dorsalResultsDatabase.meanTurnOnsEmbryo(cond1);
+seTurnON_1 = dorsalResultsDatabase.seTurnOnsEmbryo(cond1);
 
-% cond2 = [dorsalResultsDatabase.DataType]=="1Dg11_2xDl" & [dorsalResultsDatabase.nc]==12;
-% dlfluobinsfff = dorsalResultsDatabase.dorsalFluoBins(cond2);
-% fracFFF = dorsalResultsDatabase.meanFracFluoEmbryo(cond2);
-% sefracFFF = dorsalResultsDatabase.seFracFluoEmbryo(cond2);
-% maxFFF = dorsalResultsDatabase.meanAllMaxFluoEmbryo(cond2);
-% semaxFFF = dorsalResultsDatabase.seAllMaxFluoEmbryo(cond2);
-% accumFFF = dorsalResultsDatabase.meanallmrnasEmbryo(cond2);
-% seaccumFFF = dorsalResultsDatabase.seallmrnasEmbryo(cond2);
 
+convFactor = 1.2371; %with optogenetics settings Venus is this much brighter than in normal settings
+
+cond2 = [dorsalResultsDatabase.DataType]=="1Dg11_noExport" & [dorsalResultsDatabase.nc]==12;
+dlfluobins_2 = dorsalResultsDatabase.dorsalFluoBins(cond2);
+dlfluobins_2 = dlfluobins_2 ./ convFactor;
+frac_2 = dorsalResultsDatabase.meanFracFluoEmbryo(cond2);
+sefrac_2 = dorsalResultsDatabase.seFracFluoEmbryo(cond2);
+max_2 = dorsalResultsDatabase.meanAllMaxFluoEmbryo(cond2);
+semax_2 = dorsalResultsDatabase.seAllMaxFluoEmbryo(cond2);
+accum_2 = dorsalResultsDatabase.meanallmrnasEmbryo(cond2);
+seaccum_2 = dorsalResultsDatabase.seallmrnasEmbryo(cond2);
+turnOn_2 = dorsalResultsDatabase.meanTurnOnsEmbryo(cond2);
+seTurnON_2 = dorsalResultsDatabase.seTurnOnsEmbryo(cond2);
 close all
 
 %% plot results
-frac2x(isnan(frac2x)) = 0;
-max2x(isnan(max2x))=0;
-accum2x(isnan(accum2x))=0;
+frac_1(isnan(frac_1)) = 0;
+max_1(isnan(max_1))=0;
+accum_1(isnan(accum_1))=0;
+frac_2(isnan(frac_2)) = 0;
+max_2(isnan(max_2))=0;
+accum_2(isnan(accum_2))=0;
 
 %results = dorsalResults{1,1}
 
 figure
 hold on
-errorbar(dlfluobins+62.5,frac2x,sefrac2x,'ro-','CapSize',0,'MarkerFaceColor','r')
-%errorbar(dlfluobins+10,fracFFF,sefracFFF,'bo-','CapSize',0,'MarkerFaceColor','b')
-%errorbar(dlfluobins,results.meanFracFluoEmbryo,results.seFracFluoEmbryo,'r-','CapSize',0)
+errorbar(dlfluobins_1+62.5,frac_1,sefrac_1,'ro-','CapSize',0,'MarkerFaceColor','r')
+errorbar(dlfluobins_2+62.5,frac_2,sefrac_2,'bo-','CapSize',0,'MarkerFaceColor','b')
 ylabel('fraction')
 ylim([0 1])
 xlim([0 3250])
 hold off
-legend('2X and FFF combined','FFF')
-title('1Dg')
+legend('2X and 1X combined','opto Dorsal')
+title('fraction active')
 
 
 figure
 hold on
-errorbar(dlfluobins+62.5,max2x,semax2x,'ro-','CapSize',0,'MarkerFaceColor','r')
+errorbar(dlfluobins_1+62.5,max_1,semax_1,'ro-','CapSize',0,'MarkerFaceColor','r')
 %errorbar(dlfluobins+10,maxFFF,semaxFFF,'bo-','CapSize',0,'MarkerFaceColor','b')
-%errorbar(dlfluobins,results.meanAllMaxFluoEmbryo,results.seAllMaxFluoEmbryo,'r-','CapSize',0)
+errorbar(dlfluobins_2+62.5,max_2,semax_2,'bo-','CapSize',0,'MarkerFaceColor','b')
 ylabel('max fluo')
 ylim([0 600])
 xlim([0 3250])
 hold off
-legend('2X and FFF combined','FFF')
-title('1Dg')
+legend('2X and 1X combined','opto Dorsal')
+title('maximum fluorescence')
 
 
 figure
 hold on
-errorbar(dlfluobins+62.5,accum2x,seaccum2x,'ro-','CapSize',0,'MarkerFaceColor','r')
-%errorbar(dlfluobins+10,accumFFF,seaccumFFF,'bo-','CapSize',0,'MarkerFaceColor','b')
-%errorbar(dlfluobins,results.meanallmrnasEmbryo,results.seallmrnasEmbryo,'r-','CapSize',0)
+errorbar(dlfluobins_1+62.5,accum_1,seaccum_1,'ro-','CapSize',0,'MarkerFaceColor','r')
+errorbar(dlfluobins_1+62.5,accum_2,seaccum_2,'bo-','CapSize',0,'MarkerFaceColor','b')
 ylabel('accumulated mRNA')
 ylim([0 1200])
 xlim([0 3250])
 hold off
-legend('2X and FFF combined','FFF')
-title('1Dg')
+legend('2X and 1X combined','opto Dorsal')
+title('accumulated fluorescence')
 
 
 figure
 hold on
-errorbar(dlfluobins+62.5,turnOn2x,seTurnON2x,'ro-','CapSize',0,'MarkerFaceColor','r')
-%errorbar(dlfluobins+10,accumFFF,seaccumFFF,'bo-','CapSize',0,'MarkerFaceColor','b')
-%errorbar(dlfluobins,results.meanallmrnasEmbryo,results.seallmrnasEmbryo,'r-','CapSize',0)
+errorbar(dlfluobins_1+62.5,turnOn_1,seTurnON_1,'ro-','CapSize',0,'MarkerFaceColor','r')
+errorbar(dlfluobins_1+62.5,turnOn_2,seTurnON_2,'bo-','CapSize',0,'MarkerFaceColor','b')
 ylabel('turn on time')
 ylim([0 10])
 xlim([0 3250])
 hold off
-legend('2X and FFF combined','FFF')
-title('1Dg')
+legend('2X and 1X combined','opto Dorsal')
+title('turn on time')
 
 
 
