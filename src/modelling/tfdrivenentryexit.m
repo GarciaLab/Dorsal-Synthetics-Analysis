e5c0ceeaf2cf2eda2b1cbac2ed9659b7e8dff354
ArmandoReimer <armandoreimer@gmail.com>
2021-01-04 11:48:06 -0800

    improved plotting to tfdrivenentryexit

--- src/modelling/tfdrivenentryexit.m
+++ src/modelling/tfdrivenentryexit.m
@@ -266,9 +266,173 @@ for m = 1:length(cs)
 end
 
 
-
 figure
-scatter(factive(:,:,:,:,:), mfpts(:,:,:,:,:))
+scatter(factive(:), mfpts(:))
+hold on
+rectangle('Position',[.1 3.5 .9 3.5], 'LineWidth', 2, 'EdgeColor', 'r', 'Curvature',0.2) %lower left corner x&y, width, height
 xlabel('factive')
 ylabel('mean turn on (min)')
+
+%%
+
+figure; 
+tiledlayout('flow')
+for k = 1:length(pi2s)
+%dl kd pi1 c pi2
+    nexttile;
+    factivetemp = factive(:,:, :, :, k);
+    mfptstemp = mfpts(:,:, :, :, k);
+
+    scatter(factivetemp(:), mfptstemp(:))
+    hold on
+    fmin = .1;
+    fmax = 10;
+    tmin = 3.5;
+    tmax = 7;
+    rectangle('Position',[.1 3.5 .9 3.5], 'LineWidth', 2, 'EdgeColor', 'r', 'Curvature',0.2) %lower left corner x&y, width, height
+    xlabel('factive')
+    ylabel('mean turn on (min)')
+    
+    [in,on] = inpolygon(factivetemp(:),mfptstemp(:),[.1 .1 1 1],[3.5 7 7 3.5]);
+    title(pi2s(k))
+
+end
+
+
+figure; 
+tiledlayout('flow')
+for k = 1:length(pi1s)
+%dl kd pi1 c pi2
+    nexttile;
+    factivetemp = factive(:,:,k, :, :);
+    mfptstemp = mfpts(:,:, k, :, :);
+
+    scatter(factivetemp(:), mfptstemp(:))
+    hold on
+    fmin = .1;
+    fmax = 10;
+    tmin = 3.5;
+    tmax = 7;
+    rectangle('Position',[.1 3.5 .9 3.5], 'LineWidth', 2, 'EdgeColor', 'r', 'Curvature',0.2) %lower left corner x&y, width, height
+    xlabel('factive')
+    ylabel('mean turn on (min)')
+    
+    [in,on] = inpolygon(factivetemp(:),mfptstemp(:),[.1 .1 1 1],[3.5 7 7 3.5]);
+    title(pi1s(k))
+
+end
+
+figure; 
+tiledlayout('flow')
+for k = 1:length(cs)
+%dl kd pi1 c pi2
+    nexttile;
+    factivetemp = factive(:,:,:, k, :);
+    mfptstemp = mfpts(:,:, :, k, :);
+
+    scatter(factivetemp(:), mfptstemp(:))
+    hold on
+    fmin = .1;
+    fmax = 10;
+    tmin = 3.5;
+    tmax = 7;
+    rectangle('Position',[.1 3.5 .9 3.5], 'LineWidth', 2, 'EdgeColor', 'r', 'Curvature',0.2) %lower left corner x&y, width, height
+    xlabel('factive')
+    ylabel('mean turn on (min)')
+    
+    [in,on] = inpolygon(factivetemp(:),mfptstemp(:),[.1 .1 1 1],[3.5 7 7 3.5]);
+    title(cs(k))
+
+end
+
+dt = mfpts(:, 10, :, :, :) - mfpts(:, 4, :, :, :); %kd(10)=10k, kd(4)=400
+
+dt = repmat(dt, [1 length(kds) 1 1 1]);
+
+figure; 
+tiledlayout('flow')
+for k = 1:length(pi2s)
+%dl kd pi1 c pi2
+    nexttile;
+    factivetemp = factive(:,:, :, :, k);
+    mfptstemp = mfpts(:,:, :, :, k);
+    scatter(factivetemp(:), mfptstemp(:))
+    hold on
+    fmin = .1;
+    fmax = 10;
+    tmin = 3.5;
+    tmax = 7;
+    rectangle('Position',[.1 3.5 .9 3.5], 'LineWidth', 2, 'EdgeColor', 'r', 'Curvature',0.2) %lower left corner x&y, width, height
+    xlabel('factive')
+    ylabel('mean turn on (min)')
+    
+    [in,on] = inpolygon(factivetemp(:),mfptstemp(:),[.1 .1 1 1],[3.5 7 7 3.5]);
+    title(pi2s(k))
+
+end
+
+
+%%
+figure
+
+scatter3(factive(:), mfpts(:), dt(:))
+hold on
+fmin = .1;
+fmax = 10;
+tmin = 3.5;
+tmax = 7;
+deltatmin = 0;
+deltatmax = 3;
+% rectangle('Position',[.1 3.5 .9 3.5], 'LineWidth', 2, 'EdgeColor', 'r', 'Curvature',0.2) %lower left corner x&y, width, height
+% plot3( [.1 .1 1 1 .1], [3.5 7 7 3.5 3.5], [0 0 3 3 0], 'LineWidth', 3, 'Color', 'r' )
+[X,Y,Z] = ellipsoid(.55, 5.25, 1.5, .45, 1.75, 1.5);
+s = surf(X,Y,Z,'FaceAlpha',0.5, 'FaceColor', 'r');
+s.EdgeColor = 'none';
+
+xlabel('factive')
+ylabel('mean turn on (min)')
+zlabel('delta t')
+
+[in,on] = inpolygon(factivetemp(:),mfptstemp(:),[.1 .1 1 1],[3.5 7 7 3.5]);
+
+
+% save('C:\Users\Armando\Dropbox\DorsalSyntheticsDropbox\tfentryexit_paramsearch.mat')
+
+%%
+
+load('C:\Users\Armando\Dropbox\DorsalSyntheticsDropbox\tfentryexit_paramsearch.mat')
+
+factive(isnan(mfpts)) = [];
+dt(isnan(mfpts)) = [];
+mfpts(isnan(mfpts)) = [];
+
+factive(isnan(dt)) = [];
+mfpts(isnan(dt)) = [];
+dt(isnan(dt)) = [];
+
+qx = factive(:);
+qy = mfpts(:);
+qz = dt(:);
+
+shp = alphaShape(factive(:), mfpts(:), dt(:),Inf,'HoleThreshold',1E30 );
+
+figure;
+plot(shp)
+hold on
+in = inShape(shp,qx, qy, qz);
+scatter3(qx(in),qy(in),qz(in),'r.')
+scatter3(qx(~in),qy(~in), qz(~in),'b.')
+
+xlabel('factive')
+ylabel('mean turn on (min)')
+zlabel('delta t')
+legend('convex hull', 'viable parameters', 'unphysical parameters');
+
+ax = gca;
+ax.Children(3).EdgeColor = 'none';
+ax.Children(3).FaceAlpha = .5;
+
+
+
+
 
