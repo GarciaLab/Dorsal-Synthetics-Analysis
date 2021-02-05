88729aef158f459920a14dea50776bb328323b37
Simon Alamos <simon.alamos@berkeley.edu>
2021-02-05 08:41:20 -0800

    plotting functions

--- src/getDorsalFolders.m
+++ src/getDorsalFolders.m
@@ -1,7 +1,11 @@
 function [dataFolder, resultsFolder, PreProcessedFolder, ProcessedFolder] = getDorsalFolders()
 
     configValues = csv2cell('ComputerFolders.csv', 'fromfile');
-    dataFolder = getConfigValue(configValues, 'DorsalSynthetics');
+    try
+        dataFolder = getConfigValue(configValues, 'DorsalSynthetics');
+    catch
+        dataFolder = '';
+    end
     resultsFolder = getConfigValue(configValues, 'DorsalSyntheticsDropbox');
     PreProcessedFolder = [dataFolder, filesep, 'PreProcessedData'];
     ProcessedFolder = [dataFolder, filesep, 'ProcessedData'];
