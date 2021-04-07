f6382ef97b68f16f2af2c1a61b4b5a474d04fa33
Simon Alamos <simon.alamos@berkeley.edu>
2021-04-07 07:51:59 -0700

    for fitting

--- src/Simon_figures_plots/fiveOffSteps.m
+++ src/Simon_figures_plots/fiveOffSteps.m
@@ -2,6 +2,7 @@ function fraction_onset = fiveOffSteps(dorsalVals,c,kd,n_entry,n_off,piEntry,piE
 
 t = linspace(0,12,120);
 k = (c*(dls./kd) ./ (1 + dls./kd));
+endOfCycle = tCycle
 
 for i = 1:length(k)
     k1=k(i);
@@ -14,7 +15,7 @@ for i = 1:length(k)
 
     y6 = 1+(1/24).*exp(1).^ ((-1).*k1.*t) .* ((-24)+(-1).*k1.*t.*(24+k1.*t.*(12+k1.*t.*(4+k1.*t))));
     y6 = 100*y6;
-    fraction_onset(i,1) = sum(diff(y6(1:81)).*t(1:80))/sum(diff(y6(1:81)));
+    fraction_onset(i,1) = sum(diff(y6(1:tCycle+1)).*t(1:tCycle))/sum(diff(y6(1:tCycle+1)));
     fraction_onset(i,2) = y6(80)./100;
     
 %     tot = y1+y2+y3+y4+y5+(y6/100)
